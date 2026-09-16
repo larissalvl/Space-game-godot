4033e10e0bfa9d61a04f3f3f1cdbc259f8760941
Larissa <larissav.lemos2004@gmail.comm>
2026-09-16 14:59:06 -0300

    Incluir opção de acelerar a nave

--- nave.gd
+++ nave.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
-var velNave = 250
-var velAngular = PI
+var velNaveBase = 250
+var velNaveAtual = velNaveBase
+var velAngularBase = PI/2
+var velAngularAtual = velAngularBase
 
 func _ready():
 	var sprite = $AnimatedSprite2D
@@ -10,12 +12,18 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_pressed("paraEsquerda"):
-		rotation -= velAngular * delta * 1
+		rotation -= velAngularAtual * delta
 	if Input.is_action_pressed("paraDireita"):
-		rotation += velAngular * delta * 1
+		rotation += velAngularAtual * delta
 	if Input.is_action_pressed("paraTras"):
-		var movNave = Vector2.DOWN.rotated(rotation) * velNave
+		var movNave = Vector2.DOWN.rotated(rotation) * velNaveAtual
 		position += movNave * delta
 	if Input.is_action_pressed("paraFrente"):
-		var movNave = Vector2.UP.rotated(rotation) * velNave
+		var movNave = Vector2.UP.rotated(rotation) * velNaveAtual
 		position += movNave * delta
+	if Input.is_action_pressed("Acelerar"):
+		velNaveAtual = 500
+		velAngularAtual = velAngularBase * 2
+	else:
+		velNaveAtual = velNaveBase
+		velAngularAtual = velAngularBase
